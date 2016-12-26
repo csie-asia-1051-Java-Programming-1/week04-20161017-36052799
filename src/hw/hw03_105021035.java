9a90be943812ac7d42b1336b03b85f615e6ba64e
36052799 <105021035@live.asia.edu.tw>
2016-12-26 15:31:57 +0800

    Update and rename hw03_1050210XX.java to hw03_105021035.java

---
+++ src/hw/hw03_105021035.java
@@ -0,0 +1,25 @@
+package hw;
+import java.util.Scanner;
+
+public class hw03_1050210XX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scn = new Scanner(System.in);
+		int a=0, b=0,c=0,i=0;
+		while(i!=999){
+			i=scn.nextInt();
+			if(i>0 && i!=999){
+				a=a+i;
+			}else{
+				if(i==0){
+					b=b+i;
+				}else{
+					if(i<0){
+						c=c+i;
+					}
+				}
+			}System.out.println("不ˊ會) ;
+					    }
+
+}
